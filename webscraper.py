854a6383e14c40fbc97c60336cef778ef9fbd691
TomassRozenlauks <Tomass-Kristers.Ignatjevs-Rozenlauks@edu.rtu.lv>
2025-05-13 22:44:28 +0300

    Uploading the file
    - Used the Ikea template as a base for the webscraper
    - Team roster addresses have to be manually inputted into the code
    - Code outputs every player from the two teams alongside their position
    - Code counts the amount of goalies, defensemen, forwards and players in general on each team.
    - Code saves both team's roster players to compare
    - If any one player is the same name & position on both teams, the player is outputted as being one of the players on both teams.

---
+++ webscraper.py
@@ -0,0 +1,98 @@
+import requests
+from bs4 import BeautifulSoup
+
+default_adrese = "https://www.eliteprospects.com/team/673/dinamo-riga"
+
+
+
+adrese = "https://www.eliteprospects.com/team/1645/latvia-u20/2023-2024"
+adrese_2 = "https://www.eliteprospects.com/team/1645/latvia-u20"
+
+
+
+playerCount = 0
+goalieCount = 0
+defensemenCount = 0
+forwardCount = 0
+
+playerCount_2 = 0
+goalieCount_2 = 0
+defensemenCount_2 = 0
+forwardCount_2 = 0
+
+roster_1 = []
+roster_2 = []
+
+print("=============== 1ST TEAM ROSTER =================")
+# SCRAPING FIRST ROSTER
+lapa = requests.get(adrese)
+# print(lapa.status_code)
+if lapa.status_code == 200:
+    lapas_saturs = BeautifulSoup(lapa.content, "html.parser")
+    # print(lapas_saturs.prettify())
+    atradu_Player = lapas_saturs.find_all(class_="Roster_player__e6EbP")
+    #print(len(atradu_Player))
+    #print(atradu_Player)
+    for player in atradu_Player:
+        playerLink = player.find(class_="TextLink_link__RhSiC")
+        playerCount += 1
+        if playerLink:
+            fulltext = playerLink.get_text(strip=True)
+            if '(' in fulltext and ')' in fulltext:
+                name = fulltext.split("(")[0]
+                position = fulltext.split("(")[-1].strip(")")
+                if position[0] == "G":
+                    goalieCount += 1
+                elif position[0] == "D":
+                    defensemenCount += 1
+                else:
+                    forwardCount += 1
+                roster_1.append({"Name": name, "Position": position})
+            #print(fulltext)
+            print(f"Name: {name}, Position: {position}")
+
+print("Total goalie count: " + str(goalieCount))
+print("Total defensemen count: " + str(defensemenCount))
+print("Total forward count: " + str(forwardCount))
+print("Total player count: " + str(playerCount))
+#print(roster_1)
+
+# SCRAPING 2ND ROSTER
+print("=============== 2ND TEAM ROSTER =================")
+lapa = requests.get(adrese_2)
+# print(lapa.status_code)
+if lapa.status_code == 200:
+    lapas_saturs = BeautifulSoup(lapa.content, "html.parser")
+    # print(lapas_saturs.prettify())
+    atradu_Player = lapas_saturs.find_all(class_="Roster_player__e6EbP")
+    #print(len(atradu_Player))
+    #print(atradu_Player)
+    for player in atradu_Player:
+        playerLink = player.find(class_="TextLink_link__RhSiC")
+        playerCount_2 += 1
+        if playerLink:
+            fulltext = playerLink.get_text(strip=True)
+            if '(' in fulltext and ')' in fulltext:
+                name = fulltext.split("(")[0]
+                position = fulltext.split("(")[-1].strip(")")
+                if position[0] == "G":
+                    goalieCount_2 += 1
+                elif position[0] == "D":
+                    defensemenCount_2 += 1
+                else:
+                    forwardCount_2 += 1
+                roster_2.append({"Name": name, "Position": position})
+            #print(fulltext)
+            print(f"Name: {name}, Position: {position}")
+
+print("Total goalie count: " + str(goalieCount_2))
+print("Total defensemen count: " + str(defensemenCount_2))
+print("Total forward count: " + str(forwardCount_2))
+print("Total player count: " + str(playerCount_2))
+#print(roster_2)
+print("=============== PLAYERS ON BOTH TEAMS =================")
+
+for i in range(len(roster_1)):
+    for j in range(len(roster_2)):
+        if roster_1[i]==roster_2[j]:
+            print(roster_1[i])
